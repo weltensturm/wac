22a27bc3359bed463a3b88491002ac21cb7f5f27
Robert <weltensturm@gmail.com>
2013-04-04 00:45:41 +0200

    added new experimental base

--- WAC Aircraft/lua/autorun/server/wac_aircraft.lua
+++ WAC Aircraft/lua/autorun/server/wac_aircraft.lua
@@ -1,97 +1,8 @@
-<<<<<<< HEAD
 
 include "wac/aircraft.lua"
---include "wac/servertag.lua"
 
 AddCSLuaFile("autorun/client/wac_aircraft.lua");
 AddCSLuaFile("autorun/client/wac_aircraft_dev.lua");
-
---wac.tag.add("wacAir")
-
-wac.aircraft.cvars = {
-	startSpeed = CreateConVar("wac_air_startspeed", 1, {FCVAR_ARCHIVE}),
-	doubleTick = CreateConVar("wac_air_doubletick", 0, {FCVAR_ARCHIVE}),
-}
-
-wac.hook("SetPlayerAnimation", "wac_cl_heliseat_animation", function(pl, anim)
-	 if pl:InVehicle() then
-	 local v = pl:GetVehicle()
-		if string.find(v:GetModel(), "models/nova/airboat_seat") and v:GetNWEntity("wac_aircraft"):IsValid() then 
-			local seq = pl:LookupSequence("sit")	
-			pl:SetPlaybackRate(1.0)
-			pl:ResetSequence(seq)
-			pl:SetCycle(0)
-			return true
-		end
-	end
-end)
-
-
-for k,t in pairs(wac.aircraft.keys) do
-	concommand.Add("wac_air_key_" .. k, function(p, c, a)
-		if IsValid(p) and p:Alive() then
-			local e = p:GetVehicle():GetNWEntity("wac_aircraft")
-			if IsValid(e) and !e.new then
-				e:receiveInput(p, k, tonumber(a[1])==1)
-			end
-		end
-	end)
-end
-
-
-for name, type in pairs(wac.aircraft.controls) do
-	concommand.Add("wac_aircraft_input_" .. name, function(p, c, a)
-		if IsValid(p) and p:Alive() then
-			local e = p:GetVehicle():GetNWEntity("wac_aircraft")
-			if IsValid(e) then
-				e:receiveInput(p, name, tonumber(a))
-			end
-		end
-	end)
-end
-
-
-wac.hook("JoystickInitialize", "wac_air_jcon_init", function()
-
-	local tbl={
-		[WAC_AIR_LEANP]	= "Lean Forward/Back",
-		[WAC_AIR_LEANY]	= "Turn Left/Right",
-		[WAC_AIR_LEANR]	= "Roll Left/Right",
-		[WAC_AIR_UPDOWN] ="Thrust",
-		[WAC_AIR_START] = "Turn On/Off",
-		[WAC_AIR_FIRE] ="Shoot",
-		[WAC_AIR_CAM] = "Toggle Camera",
-		[WAC_AIR_NEXTWEP] = "Next Weapon",
-		[WAC_AIR_HOVER] = "Auto Hover Toggle",
-		[WAC_AIR_EXIT] = "Exit Helicopter",
-		[WAC_AIR_FREEAIM] = "Free Aim",
-	}
-
-	for k, v in pairs(tbl) do
-		jcon.register({
-			uid = "wac_air_"..k,
-			type = "analog",
-			description = v[2],
-			category = "WAC Helicopter"
-		})
-	end
-	jcon.register({
-		uid="wac_air_"..WAC_AIR_THIRDP,
-		type="analog",
-		description="Third Person",
-		category="WAC Helicopter"
-	})
-	
-end)
-=======
-
-include "wac/aircraft.lua"
---include "wac/servertag.lua"
-
-AddCSLuaFile("autorun/client/wac_aircraft.lua");
-AddCSLuaFile("autorun/client/wac_aircraft_dev.lua");
-
---wac.tag.add("wacAir")
 
 
 -- new input
@@ -183,4 +94,4 @@ wac.hook("JoystickInitialize", "wac_air_jcon_init", function()
 	})
 	
 end)
->>>>>>> added new experimental base
+
--- WAC Aircraft/lua/entities/wac_rotor/shared.lua
+++ WAC Aircraft/lua/entities/wac_rotor/shared.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 include("wac/aircraft.lua")
 
@@ -14,22 +13,3 @@ ENT.Instructions = ""
 
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
-
-=======
-
-include("wac/aircraft.lua")
-
-ENT.Base = "base_anim"
-ENT.Type = "anim"
-
-ENT.PrintName = "Base Rotor"
-ENT.Author = wac.author
-ENT.Category = wac.aircraft.spawnCategory
-ENT.Contact = ""
-ENT.Purpose = ""
-ENT.Instructions = ""
-
-ENT.Spawnable = false
-ENT.AdminSpawnable = false
-
->>>>>>> added new experimental base
